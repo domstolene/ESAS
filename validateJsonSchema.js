cd7812f9dfcfaf832ba63500ee0253301431da11
Martin Myran <martin.myran@politiet.no>
2024-02-21 12:17:05 +0100

    Versjon 0.9 siktelse er klar

--- validateJsonSchema.js
+++ validateJsonSchema.js
@@ -17,6 +17,7 @@ const jsonSchemaFolders = [
   "kontrakter/konfliktraadet/oppdatertsaksstatus",
   "kontrakter/personundersoekelse/rekvisisjonPersonundersoekelse",
   "kontrakter/personundersoekelse/returPersonundersoekelse",
+  "kontrakter/siktelseTiltale"
 ];
 
 const jsonKodeverkFolders = ["kodeverk/felles"];
